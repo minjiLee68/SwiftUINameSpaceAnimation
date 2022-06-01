9e1481f116d13b960e1a21f5f42b6b366d4a0235
minji0608 <106049821+minji0608@users.noreply.github.com>
2022-06-01 22:58:01 +0900

    Initial Commit

---
+++ SwiftUI_TabBar_Animation/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI_TabBar_Animation
+//
+//  Created by 이민지 on 2022/06/01.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI_TabBar_Animation/SwiftUI_TabBar_AnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_TabBar_AnimationApp.swift
+//  SwiftUI_TabBar_Animation
+//
+//  Created by 이민지 on 2022/06/01.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_TabBar_AnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
